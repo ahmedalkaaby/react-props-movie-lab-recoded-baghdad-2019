acc6699c80d99c91b509795ddc03446bcb79baf5
K3be <ahmed.13.alkaaby@gmail.com>
2020-01-01 14:59:53 +0000

    Automatically backed up by Learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -24,7 +24,7 @@ else {
       <div className="card-back">
         <h3 className="title">{this.props.title}</h3>
         <span />
-        {this.generateRatingElement()} 
+        {this.generateRatingElement()}
         <span />
         <h5 className="genres">{this.props.geners.join(",")}</h5>
       </div>
